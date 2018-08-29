f834f8ed02c40b6fcd3590e1958042089b78c209
Savannah Niemeyer <savannahniemeyer@gmail.com>
2018-08-29 17:43:10 -0500

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 //this file houses all mysql connection data
 
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  port     : 3306,
-  host     : 'localhost',
-  user     : 'root',
-  password : 'testing123',
-  database : 'burgers_db'
-});
+var connection;
+  if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+   connection = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'root',
+      password : 'testing123',
+      database : 'burgers_db'
+  });
+};
  
 connection.connect(function(err) {
     if (err) {
