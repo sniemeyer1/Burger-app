f6bcce7a9e43bf071d0143e5276b74e9a004481c
Savannah Niemeyer <savannahniemeyer@gmail.com>
2018-08-31 02:00:18 -0500

    added link

--- burger_db/schema.sql
+++ burger_db/schema.sql
@@ -1,8 +1,17 @@
+CREATE DATABASE IF NOT EXISTS burgers_db;
+
 USE burgers_db;
 
-CREATE TABLE burgers(
+CREATE TABLE IF NOT EXISTS burgers(
 	id int NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chickenburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Salmonburger", FALSE);
+
+SELECT * FROM burgers;
--- config/connection.js
+++ config/connection.js
@@ -2,19 +2,15 @@
 
 var mysql      = require('mysql');
 
-var connection;
-if(process.env.JAWSDB_URL) {
-  //Heroku deployment
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  //local host
-    connection = mysql.createConnection({
-        root: 3000,
-        host: "localhost",
-        user: "root",
-        password: "testing123",
-        database: "burgers_db",
-    });
+if (process.env.JAWSDB_URL){
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    var connection = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'root',
+      password : 'testing123',
+      database : 'burgers_db'
+  });
 };
  
 connection.connect(function(err) {
